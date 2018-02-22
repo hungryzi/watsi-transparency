2d3ff3168351c68f59c2d72344c930991d4f30c9
Zi <hungryzi@gmail.com>
2018-02-21 21:47:48 -0500

    Basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Typography from 'typography';
+import theme from 'typography-theme-parnassus';
+import {Helmet} from "react-helmet";
+
 import logo from './logo.svg';
 import './App.css';
+
+const typography = new Typography(theme);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>Watsi's Transparency</title>
+          <style>{typography.toString()}</style>
+        </Helmet>
         <header className="App-header">
           <h1 className="App-title">Watsi's Transparency</h1>
         </header>
